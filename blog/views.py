cc900c4990dbe5b4f192cc2fb7a5d0d178fc14f2
Slawomir Dyk <slawomir.dyk@astrosat.net>
2021-08-24 15:16:26 +0100

    Add comment model

--- blog/views.py
+++ blog/views.py
@@ -2,8 +2,12 @@ from django.db.models import query
 from django.shortcuts import render
 from django.views.generic.base import TemplateView
 from django.views.generic import ListView, DetailView
+from django.views import View
+from django.http import HttpResponseRedirect
+from django.urls import reverse
 
 from .models import Post
+from .forms import CommnetForm
 
 # Create your views here.
 
@@ -14,9 +18,34 @@ class AllPostsView(ListView):
     ordering = ["-date"]
 
 
-class SinglePostView(DetailView):
-    template_name = "blog/post-detail.html"
-    model = Post
+class SinglePostView(View):
+    def get(self, request, slug):
+        post = Post.objects.get(slug=slug)
+        context = {
+            "post": post,
+            "post_tag": post.tags.all(),
+            "comment_form": CommnetForm(),
+            "comments": post.comments.all().order_by("-id")
+        }
+        return render(request, "blog/post-detail.html", context)
+
+    def post(self, request, slug):
+
+        comment_form = CommnetForm(request.POST)
+        post = Post.objects.get(slug=slug)
+        if comment_form.is_valid():
+            comment = comment_form.save(commit=False)
+            comment.post = post
+            comment.save()
+            return HttpResponseRedirect(reverse("post-detail-page", args=[slug]))
+
+        context = {
+            "post": post,
+            "post_tag": post.tags.all(),
+            "comment_form": CommnetForm(),
+            "comments": post.comments.all().order_by("-id")
+        }
+        return render(request, "blog/post-detail.html", context)
 
 
 class StartingPageView(ListView):
